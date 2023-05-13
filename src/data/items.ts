06ac090804fd18696819a47c0fe76257f2144000
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-13 15:01:15 -0300

    feat: create a fake data with fake items

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/item";
+
+export const item: Item[] = [
+    {date: new Date(2023, 3, 10), category: 'food', title: 'McDonalds', value: 23.45},
+    {date: new Date(2023, 4, 9), category: 'food', title: 'Burger King', value: 30},
+    {date: new Date(2023, 4, 13), category: 'rent', title: 'Aluguel apt', value: 600},
+    {date: new Date(2023, 4, 13), category: 'salary', title: 'Salário ACM', value: 1500}
+];
