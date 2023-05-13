49be2bfed83ea04f3bc3d8203fb294eac7e592bc
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-13 19:30:17 -0300

    feat(TableItem): each category has own bg color

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,6 +1,7 @@
 import './styles.scss';
 import { formatDate } from '../../helpers/dateFilter';
 import { Item } from '../../types/Item';
+import { categories } from '../../data/categories';
 
 type Props = {
     item: Item;
@@ -10,7 +11,11 @@ export const TableItem = ({ item }: Props) => {
     return (
         <tr className='tableItem__body'>
             <td>{formatDate(item.date)}</td>
-            <td>{item.category}</td>
+            <td>
+                <div className={`category ${categories[item.category].color}`}>
+                    {categories[item.category].title}
+                </div>
+            </td>
             <td>{item.title}</td>
             <td>R${item.value}</td>
         </tr>
