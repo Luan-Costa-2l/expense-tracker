90131ec45550fa07717dcf902fd54f1df7b9aa38
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-13 19:10:39 -0300

    feat(App): add table area component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import './App.styles.scss';
 import { TableArea } from './components/TableArea';
+import { item } from './data/items';
 
 
 const App = () => {
+  const [list, setList] = useState(item);
+
   return (
     <div className="container">
       <header className="header">
@@ -14,7 +18,7 @@ const App = () => {
 
         {/* área de inserção */}
 
-        <TableArea />
+        <TableArea list={list} />
       </main>
     </div>
   )
