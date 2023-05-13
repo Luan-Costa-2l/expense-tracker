e7f3ca6a85c97dec74528139117fa2bf46c9d7b7
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-13 14:18:55 -0300

    start project with vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.styles.scss'
+const App = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>Sistema Financeiro</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App;
