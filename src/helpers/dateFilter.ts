4b269f8f7ee0a6f8cc3038baa504b1ea25c72a26
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-13 18:50:31 -0300

    feat(helpers): create a format date function

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -1,3 +1,10 @@
 const fixZero = (number: number) => {
     return number < 10 ? '0'+number : number;
 }
+export const formatDate = (date: Date) => {
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+
+    return `${fixZero(day)}/${fixZero(month)}/${fixZero(year)}`;
+}
