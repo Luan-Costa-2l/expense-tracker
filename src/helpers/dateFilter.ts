31cd1d76cde31c68c983b0310355df4113556550
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-13 21:24:01 -0300

    feat(helpers): get current month function

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -1,6 +1,12 @@
+export const getCurrentMonth = () => {
+    const now = new Date();
+    return `${now.getFullYear()}-${now.getMonth()}`;
+}
+
 const fixZero = (number: number) => {
     return number < 10 ? '0'+number : number;
 }
+
 export const formatDate = (date: Date) => {
     let year = date.getFullYear();
     let month = date.getMonth() + 1;
