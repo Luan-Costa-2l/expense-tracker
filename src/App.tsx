9d4477a09c49c19ef8673bd1b329c0169148c5d5
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-15 16:52:49 -0300

    feat(app): get income and expense value

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,33 @@ import { item } from './data/items';
 import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
 import { Item } from './types/Item';
 import { InfoArea } from './components/InfoArea';
+import { categories } from './data/categories';
 
 
 const App = () => {
   const [list, setList] = useState(item);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList( filterListByMonth(item, currentMonth) );
   }, [list, currentMonth]);
+
+  useEffect(() => {
+    let incomeValue = 0;
+    let expenseValue = 0;
+    for (let item of filteredList) {
+      if (categories[item.category].expense) {
+        expenseValue += item.value;
+      } else {
+        incomeValue += item.value;
+      }
+    }
+    setIncome(incomeValue);
+    setExpense(expenseValue);
+  }, [filteredList]);
 
   const handleMonthChange = (newMonth: string) => {
     setCurrentMonth(newMonth);
